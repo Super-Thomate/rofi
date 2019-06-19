2c83e0e23e28a94e95d0a94d7a288a9e5dfc4295
thomate <thomate@thomate>
2019-06-19 14:40:48 +0000

    feature 2 TEST

--- cogs/roles/roles.py
+++ cogs/roles/roles.py
@@ -19,7 +19,15 @@ class Roles(commands.Cog):
         if role.id == role_id:
           return True
       return False
-
+    def get_new_nick (member):
+      new_nick = member.display_name.replace('🔥','').replace('⭐','').replace('💖','')
+      if has_role (member, self.roles["ar"]):
+        new_nick += " 🔥"
+      if has_role (member, self.roles["suf"]):
+        new_nick += " ⭐"
+      if has_role (member, self.roles["star"]):
+        new_nick += " 💖"
+      return new_nick
     if (     not has_role (before, self.roles["work_team"])
          and not has_role (after, self.roles["work_team"])
          and (    has_role (after, self.roles["suf_work_team"])
@@ -51,6 +59,22 @@ class Roles(commands.Cog):
         await after.remove_roles(role_to_remove)
       except Exception as e:
         print (f"ERROR: {type(e).__name__} - {e}")
+
+    if (    (not has_role (before, self.roles["ar"])   and has_role (after, self.roles["ar"]))
+         or (not has_role (before, self.roles["suf"])  and has_role (after, self.roles["suf"])) 
+         or (not has_role (before, self.roles["star"]) and has_role (after, self.roles["star"]))
+         or (has_role (before, self.roles["ar"])   and not has_role (after, self.roles["ar"]))
+         or (has_role (before, self.roles["suf"])  and not has_role (after, self.roles["suf"]))
+         or (has_role (before, self.roles["star"]) and not has_role (after, self.roles["star"]))
+       ):
+      """ add/remove emoji
+      """
+      print ("add/remove emoji")
+      try:
+        await after.edit(nick= get_new_nick (after))
+      except Exception as e:
+        print (f"ERROR: {type(e).__name__} - {e}")
+  
   @commands.command(name="roles")
   async def print_roles(self, ctx, *, member: discord.Member = None):
     member = member or ctx.author
